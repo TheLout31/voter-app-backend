660f37d55bae5dc2b9768b0e84140e35055b77e6
Imran Jaleel <imranjaleel2331@gmail.com>
2025-09-29 14:10:14 +0530

    made few changes

--- config/mongodb.config.js
+++ config/mongodb.config.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose");
 
 const connectToDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("connected to MongoDB !!!!");
   } catch (err) {
     console.log("Error connecting to MongoDB", err);
