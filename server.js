fda0d4409130c95ab97c2f26dba098569ff45cde
Imran Jaleel <imranjaleel2331@gmail.com>
2025-09-29 14:01:01 +0530

    made few changes

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const connectToDB = require("./config/mongodb.config");
 const UserRouter = require("./routes/auth.routes");
-const  {loginLimiter}  = require("./middlewares/rateLimiter");
+const { loginLimiter } = require("./middlewares/rateLimiter");
 const ProfileRouter = require("./routes/profile.routes");
 const VotingRouter = require("./routes/voting.routes");
 const ElectionRouter = require("./routes/election.routes");
 
-
 const app = express();
 const PORT = 3000;
 app.use(cors());
+app.use(express.json());
 
 connectToDB();
 
@@ -18,21 +18,15 @@ app.get("/", (req, res) => {
   res.json({ message: "Server working fine!!!" });
 });
 
-app.use(express.json());
-
 app.use("/auth", UserRouter);
-app.use('/profile', ProfileRouter);
-app.use('/uploads', express.static('uploads'));
-app.use("/vote", VotingRouter)
-app.use("/elect",ElectionRouter)
-
-
+app.use("/profile", ProfileRouter);
+app.use("/uploads", express.static("uploads"));
+app.use("/vote", VotingRouter);
+app.use("/elect", ElectionRouter);
 
 app.post("/auth/login", loginLimiter, (req, res, next) => next());
 
-
-
-// app.listen(PORT, () => {
-//   console.log(`Server running on http://localhost:${PORT}`);
-// });
-module.exports = app
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+module.exports = app;
