04954b9dc460a6932ae1797a3d439fbceb3acf68
Imran Jaleel <imranjaleel2331@gmail.com>
2025-09-29 12:48:30 +0530

    fixed few issues

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -1,9 +1,9 @@
-const rateLimit = require('express-rate-limit');
+const rateLimit = require("express-rate-limit");
 
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 10, // limit each IP to 10 requests per windowMs
-  message: { error: 'Too many attempts, please try again later.' },
+  message: { error: "Too many attempts, please try again later." },
 });
 
-module.exports = { loginLimiter };
+module.exports = {loginLimiter};
